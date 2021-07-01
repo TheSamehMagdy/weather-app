178494f1c0837fd6571c5b9f7ce8c7c56aaa0878
Sameh Magdy <thesamehmagdy@gmail.com>
2021-07-02 01:38:31 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+// Define paths for Express config
+const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Set up handlebards and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Set up static directory to serve
+app.use(express.static(publicPath));
+
+// Define routes
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Sameh Magdy'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About Me',
+		name: 'Sameh Magdy'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help',
+		name: 'Sameh Magdy'
+	});
+});
+
+app.get('/weather', (req, res) => {
+	if (!req.query.address) {
+		return res.send({
+			error: 'Please provide an address.'
+		});
+	}
+
+	geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+		if (error) {
+			return res.send({ error });
+		}
+		forecast(latitude, longitude, (error, forecastString) => {
+			if (error) {
+				return res.send({ error });
+			}
+			res.send({
+				location: location,
+				forecast: forecastString
+			});
+		});
+	});
+});
+
+app.get('/help/*', (req, res) => {
+	res.render('404', {
+		title: '404 Not Found',
+		name: 'Sameh Magdy',
+		error: 'Help article not found'
+	});
+});
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: '404 Not Found',
+		name: 'Sameh Magdy',
+		error: 'Page not found'
+	});
+});
+
+app.listen(3000, () => {
+	console.log(`Server is running on port 3000`);
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=420bb3e378573914d379c12d2853bb66`;
+
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			callback('Unable to connect to weather service.');
+		} else if (body.message) {
+			callback(body.message);
+		} else {
+			const weatherData = body;
+			const forecastString = `${weatherData.weather[0].main}. It is currently ${weatherData.main
+				.temp} degrees in ${weatherData.name}.`;
+			callback(undefined, forecastString);
+		}
+	});
+};
+
+module.exports = forecast;
